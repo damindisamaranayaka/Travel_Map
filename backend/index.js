53459bf2bb263f2a5a7a438478f5e68d28fd9b15
damindisamaranayaka <damindisamaranayaka2@gmail.com>
2025-03-18 07:17:12 +0530

    remove some comment

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 app.use(express.json())
 app.use(cors({
-    origin: 'http://localhost:3000'  // Allow requests from your frontend
+    origin: 'http://localhost:3000'  // Allow requests from frontend
   }));
 
 mongoose
